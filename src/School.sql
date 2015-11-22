37f73e092dcd1d56967220fd8bda8e95a0be1b05
Jacob Doiron <jdoiron@smcm.edu>
2015-11-21 22:46:26 -0500

    SQL corrections

--- src/School.sql
+++ src/School.sql
@@ -4,7 +4,7 @@ CREATE TABLE `student` (
   `StudentLN` varchar(255) DEFAULT NULL,
   `StudentCI` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`StudentID`)
-)
+);
 
 INSERT INTO `student` VALUES (12345,'John','Doe','3015555555'),(54321,'Jane','Brown','5555555555'),(11111,'Femi','Oyenusi','1111111111'),(22222,'Jacob','Doiron','2222222222'),(33333,'Joe','Miller','3333333333'),(44444,'Benjamin','Dodson','4444444444'),(55555,'Amy','Whittington','5555555555'),(66666,'Caroline','Orlando','6666666666'),(77777,'Nicholas','Custodio','7777777777'),(88888,'Caleb','Jardeleza','9999999999'),(99999,'Mark','Hardesty','0000000000');
 
@@ -17,7 +17,7 @@ CREATE TABLE `stuschedule` (
   `5Class` varchar(3) DEFAULT NULL,
   `6Class` varchar(3) DEFAULT NULL,
   PRIMARY KEY (`StudentID`)
-)
+);
 
 INSERT INTO `stuschedule` VALUES (12345,'100','251','302','111','121','300'),(54321,'251','111','100','300','121','302'),(11111,'251','111','300','100','121','302'),(22222,'302','111','100','300','251','121'),(33333,'251','111','100','300','121','302'),(44444,'251','121','111','100','300','302'),(55555,'251','111','100','300','121','302'),(66666,'251','121','100','302','111','300'),(77777,'251','111','100','300','121','302'),(88888,'111','251','100','300','121','302'),(99999,'251','111','300','100','121','302');
 
@@ -28,6 +28,6 @@ CREATE TABLE `teacher` (
   `Subject` varchar(255) DEFAULT NULL,
   `Classroom` varchar(3) DEFAULT NULL,
   PRIMARY KEY (`TeacherID`)
-)
+);
 
 INSERT INTO `teacher` VALUES (11223,'Bob','Smith','Mathematics','100'),(12123,'Jim','Johnson','English','251'),(22123,'Lindsay','Jamieson','Computer Science','111'),(11122,'Christopher','Black','Art','300'),(22223,'Matt','Jones','Chemistry','121'),(11112,'Bob','Smith','History','302');
--- src/School_query.sql
+++ src/School_query.sql
@@ -3,5 +3,5 @@ select TeacherID from teacher where Classroom = 100;
 select TeacherID, TeacherLN, Subject from teacher;
 select 1Class, 2Class, 3Class, 4Class, 5Class, 6Class from stuschedule where StudentID = 12345;
 select StudentID from student;
-select StudentID from student where StudentCI = '3018675309';
+select StudentID from student where StudentCI = '3015555555';
 select Classroom from teacher where TeacherID = 22123;
